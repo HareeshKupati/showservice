4ece9757dffe6b4c3b6bd567e7bee22a8a677f71
Hareesh Kupate <hbkupati@gmail.com>
2026-02-06 23:21:27 +0530

    Query Modification

--- src/main/java/com/hbk/showservice/repository/MovieTheatreShowRepository.java
+++ src/main/java/com/hbk/showservice/repository/MovieTheatreShowRepository.java
@@ -2,6 +2,7 @@ package com.hbk.showservice.repository;
 
 import com.hbk.corefw.repository.CoreRepository;
 import com.hbk.showservice.jdo.MovieTheatreShowJDO;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -12,6 +13,7 @@ import java.util.List;
 @Repository
 public interface MovieTheatreShowRepository extends CoreRepository<MovieTheatreShowJDO, Long> {
 
+    @EntityGraph(attributePaths = {"movieTheatreShowSeats","showTime"})
     @Query("select mts from MovieTheatreShowJDO mts JOIN FETCH mts.movieTheatreShowSeats mtss " +
             "where mts.movieId = :movieId and mts.showTime.id = :showId and mts.theatreId = :theatreId " +
             "and mts.showDate = :showDate and mtss.seatId IN :seatIds")
